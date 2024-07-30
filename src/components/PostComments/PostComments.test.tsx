ae2f49cfcd7ad6c5e4ba4b4d20d612a3b159891c
Kayky Dion <kayky.dion@gmail.com>
2024-07-30 20:18:58 -0300

    Testando a inserção de comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
+import PostComment from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("Teste para o componente PostComment", () => {
+  it("Deve renderizar o componente corretamente", () => {
+    render(<PostComment />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+  it("Deve renderizar dois comentários", () => {
+    const { debug } = render(<PostComment />);
+
+    const textarea = screen.getByTestId("textarea");
+    const form = screen.getByTestId("comment-form");
+
+    fireEvent.change(textarea, {
+      target: { value: "Comentário 1" },
     });
+
+    fireEvent.submit(form);
+
+    fireEvent.change(textarea, {
+      target: { value: "Comentário 2" },
+    });
+
+    fireEvent.submit(form);
+
+    debug();
+
+    expect(screen.getAllByTestId("comment")).toHaveLength(2);
+  });
 });
